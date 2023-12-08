1689f2adbd58deec9a6bd3e6d8e7e121a9b16f4c
José María CL <chema9703@gmail.com>
2023-12-08 01:18:43 -0600

    feat: add basic endpoints to create and read empleados

--- src/pages/api/empleados/index.ts
+++ src/pages/api/empleados/index.ts
@@ -1,11 +1,45 @@
 import type { APIRoute } from 'astro'
+import { PrismaClient } from '@prisma/client'
 
-export const GET: APIRoute = async ({ request }) => {
-  const body = await request.json()
+const prisma = new PrismaClient()
+
+export const GET: APIRoute = async () => {
+  const empleados = await prisma.empleado.findMany({
+   select: {
+    noEmpleado: true,
+    nombre: true,
+    apellidos: true,
+    email: true,
+    telefono: true,
+    id: true,
+   } 
+  })
+
   return new Response(
     JSON.stringify({
-      data: []
+      data: empleados,
     }),
-    { status: 200 }
+    { status: 200, headers: { 'Content-Type': 'application/json' } }
+  )
+}
+
+export const POST: APIRoute = async ({ request }) => {
+  const body = await request.json()
+
+  const empleado = await prisma.empleado.create({
+    data: {
+      nombre: body.nombre,
+      apellidos: body.apellidos,
+      email: body.email,
+      telefono: body.telefono,
+      noEmpleado: body.noEmpleado,
+    },
+  })
+
+  return new Response(
+    JSON.stringify({
+      data: empleado,
+    }),
+    { status: 201, headers: { 'Content-Type': 'application/json' } }
   )
 }
