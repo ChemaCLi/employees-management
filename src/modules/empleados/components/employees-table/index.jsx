0d576f6a2103bfada729410e32798d4b6d9f6ef3
José María CL <chema9703@gmail.com>
2023-12-08 00:01:57 -0600

    feat: add basic UI

---
+++ src/modules/empleados/components/employees-table/index.jsx
@@ -0,0 +1,41 @@
+import React from "react"
+import { Table } from "antd"
+
+export const EmployeesTable = ({ employees = [] }) => {
+  const columns = [
+    {
+      title: "No. de empleado",
+      dataIndex: "noEmpleado",
+      key: "noEmpleado"
+    },
+    {
+      title: "Nombre",
+      dataIndex: "nombre",
+      key: "nombre"
+    },
+    {
+      title: "Apellidos",
+      dataIndex: "apellidos",
+      key: "apellidos"
+    },
+    {
+      title: "Email",
+      dataIndex: "email",
+      key: "email"
+    },
+    {
+      title: "Teléfono",
+      dataIndex: "telefono",
+      key: "telefono"
+    }
+  ]
+
+  return (
+    <Table
+      scroll={{ x: true }}
+      rowKey={record => record.id}
+      dataSource={employees}
+      columns={columns}
+    />
+  )
+}
--- src/modules/empleados/pages/empleados.page.jsx
+++ src/modules/empleados/pages/empleados.page.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import { Breadcrumb, message } from 'antd'
+import { Breadcrumb, Button, Col, Input, Row, message } from 'antd'
 import { UserLayout } from '../../shared/application/layouts/user-layout'
+import { EmployeesTable } from '../components/employees-table'
 
 export const EmpleadosPage = () => {
   return (
@@ -13,7 +14,17 @@ export const EmpleadosPage = () => {
           link: '/empleados'
         }]} />
       }>
-      <div>empleados</div>
+        <Row gutter={[16, 16]}>
+          <Col span={20}>
+            <Input.Search allowClear placeholder="Buscar por nombre" />
+          </Col>
+          <Col span={4}>
+            <Button style={{ width: '100%' }} type="primary" onClick={() => message.info('Crear empleado')}>
+              Crear empleado
+            </Button>
+          </Col>
+        </Row>
+      <EmployeesTable />
     </UserLayout>
   )
 }
